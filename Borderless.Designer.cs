7c55ff3439ebfe6d17c45c776eff04c4be6cba51
Codeusa <andrew@codeusa523.org>
2014-01-11 19:43:47 -0500

    Renamed Classes

---
+++ Borderless.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace BorderlessGaming
+{
+    partial class Borderless
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Borderless));
+            this.makeBorderlessButton = new System.Windows.Forms.Button();
+            this.selectedProcess = new System.Windows.Forms.Label();
+            this.processList = new System.Windows.Forms.ListBox();
+            this.refreshList = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // makeBorderlessButton
+            // 
+            this.makeBorderlessButton.Location = new System.Drawing.Point(224, 67);
+            this.makeBorderlessButton.Name = "makeBorderlessButton";
+            this.makeBorderlessButton.Size = new System.Drawing.Size(129, 36);
+            this.makeBorderlessButton.TabIndex = 1;
+            this.makeBorderlessButton.Text = "Make Borderless";
+            this.makeBorderlessButton.UseVisualStyleBackColor = true;
+            this.makeBorderlessButton.Click += new System.EventHandler(this.MakeBorderless);
+            // 
+            // selectedProcess
+            // 
+            this.selectedProcess.AutoSize = true;
+            this.selectedProcess.Location = new System.Drawing.Point(224, 21);
+            this.selectedProcess.Name = "selectedProcess";
+            this.selectedProcess.Size = new System.Drawing.Size(107, 13);
+            this.selectedProcess.TabIndex = 2;
+            this.selectedProcess.Text = "No Process Selected";
+            this.selectedProcess.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // processList
+            // 
+            this.processList.FormattingEnabled = true;
+            this.processList.Location = new System.Drawing.Point(-1, -1);
+            this.processList.Name = "processList";
+            this.processList.Size = new System.Drawing.Size(219, 199);
+            this.processList.TabIndex = 0;
+            this.processList.SelectedIndexChanged += new System.EventHandler(this.processList_SelectedIndexChanged);
+            // 
+            // refreshList
+            // 
+            this.refreshList.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("refreshList.BackgroundImage")));
+            this.refreshList.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.refreshList.Location = new System.Drawing.Point(224, 152);
+            this.refreshList.Name = "refreshList";
+            this.refreshList.Size = new System.Drawing.Size(33, 31);
+            this.refreshList.TabIndex = 3;
+            this.refreshList.UseVisualStyleBackColor = true;
+            this.refreshList.Click += new System.EventHandler(this.refreshList_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(263, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 31);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(302, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 31);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Borderless
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 195);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.refreshList);
+            this.Controls.Add(this.selectedProcess);
+            this.Controls.Add(this.makeBorderlessButton);
+            this.Controls.Add(this.processList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(381, 234);
+            this.MinimumSize = new System.Drawing.Size(381, 234);
+            this.Name = "Borderless";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Borderless Gaming";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button makeBorderlessButton;
+        private System.Windows.Forms.Label selectedProcess;
+        private System.Windows.Forms.ListBox processList;
+        private System.Windows.Forms.Button refreshList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Borderless.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BorderlessGaming
+{
+    public partial class Borderless : Form
+    {
+        public delegate bool WindowEnumCallback(int hwnd, int lparam);
+
+        private const int SW_HIDE = 0x00;
+        private const int SW_SHOW = 0x05;
+        private const int WS_EX_APPWINDOW = 0x40000;
+        private const int GWL_EXSTYLE = -0x14; //never want to hunt this down again
+        private const int WS_EX_DLGMODALFRAME = 0x0001;
+        private const int WS_THICKFRAME = 0x00040000;
+        private const int WS_EX_TOOLWINDOW = 0x0080;
+        private const short SWP_NOMOVE = 0X2;
+        private const short SWP_NOSIZE = 1;
+        private const short SWP_NOZORDER = 0X4;
+        private const int SWP_SHOWWINDOW = 0x0040;
+        public static uint MF_BYPOSITION = 0x400;
+        public static uint MF_REMOVE = 0x1000;
+        public static int GWL_STYLE = -16;
+        public static int WS_CHILD = 0x40000000; //child window
+        public static int WS_BORDER = 0x00800000; //window with border
+        public static int WS_DLGFRAME = 0x00400000; //window with double border but no title
+        public static int WS_CAPTION = WS_BORDER | WS_DLGFRAME; //window with a title bar 
+        public static int WS_SYSMENU = 0x00080000; //window menu
+        private readonly List<string> processDataList = new List<string>();
+        private readonly List<string> tempList = new List<string>();
+        private bool gameFound;
+        private string selectedProcessName;
+
+        public Borderless()
+        {
+            InitializeComponent();
+            PopulateList();
+        }
+
+        [DllImport("USER32.DLL")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        //Sets a window to be a child window of another window
+        [DllImport("USER32.DLL")]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        //Sets window attributes
+        [DllImport("USER32.DLL")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        //Gets window attributes
+        [DllImport("USER32.DLL")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        private static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetMenu(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern int GetMenuItemCount(IntPtr hMenu);
+
+        [DllImport("user32.dll")]
+        private static extern bool DrawMenuBar(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool RemoveMenu(IntPtr hMenu, uint uPosition, uint uFlags);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy,
+            int wFlags);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EnumWindows(WindowEnumCallback lpEnumFunc, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern void GetWindowText(int h, StringBuilder s, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(int h);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowText(IntPtr hWnd, string text);
+
+        //assorted constants needed
+
+
+        private void PopulateList() //Adds active windows to the processDataList
+        {
+            tempList.Add("Refreshing...");
+            processList.DataSource = tempList;
+            var processlist = Process.GetProcesses();
+
+            foreach (var process in processlist)
+            {
+                if (process == null)
+                {
+                    continue;
+                }
+                if (process.ProcessName.Equals("explorer"))
+                {
+                    continue;
+                }
+                if (String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    SetWindowText(process.MainWindowHandle, process.ProcessName);
+                }
+                if (process.MainWindowTitle.Length > 0)
+                {
+                    processDataList.Add(process.ProcessName);
+                }
+            }
+
+
+            UpdateList();
+        }
+
+
+        private void RemoveBorder(String procName) //actually make it frameless
+        {
+            var Procs = Process.GetProcesses();
+            foreach (var proc in Procs)
+            {
+                if (gameFound.Equals(true))
+                {
+                    gameFound = false;
+                    return;
+                }
+
+                if (proc.ProcessName.StartsWith(selectedProcessName))
+                {
+                    var pFoundWindow = proc.MainWindowHandle;
+                    var style = GetWindowLong(pFoundWindow, GWL_STYLE);
+
+                    //get menu
+                    var HMENU = GetMenu(proc.MainWindowHandle);
+                    //get item count
+                    var count = GetMenuItemCount(HMENU);
+                    //loop & remove
+                    for (var i = 0; i < count; i++)
+                    {
+                        RemoveMenu(HMENU, 0, (MF_BYPOSITION | MF_REMOVE));
+                        RemoveMenu(HMENU, 0, (MF_BYPOSITION | MF_REMOVE));
+                    }
+
+                    //force a redraw
+                    DrawMenuBar(proc.MainWindowHandle);
+                    SetWindowLong(pFoundWindow, GWL_STYLE, (style & ~WS_SYSMENU));
+                    SetWindowLong(pFoundWindow, GWL_STYLE, (style & ~WS_CAPTION));
+                    SetWindowLong(pFoundWindow, GWL_STYLE,
+                        GetWindowLong(pFoundWindow, GWL_STYLE) & ~(WS_BORDER | WS_DLGFRAME | WS_THICKFRAME));
+                    SetWindowLong(pFoundWindow, GWL_EXSTYLE,
+                        GetWindowLong(pFoundWindow, GWL_EXSTYLE) & ~WS_EX_DLGMODALFRAME);
+                    SetWindowPos(pFoundWindow, 0, 0, 0, 0, 0, SWP_NOZORDER | SWP_NOSIZE | SWP_SHOWWINDOW);
+                    //resets window to main monitor 
+                    gameFound = true;
+                }
+            }
+            if (gameFound.Equals(false))
+            {
+                MessageBox.Show("I was unable to find that application! But what do I know I'm just a robot.",
+                    "Hey, listen!", MessageBoxButtons.OK, MessageBoxIcon.Error); //just in case
+            }
+            gameFound = false;
+        }
+
+
+        private void refreshList_Click(object sender, EventArgs e) //handles refresh button
+        {
+            processList.DataSource = null;
+            processList.Items.Clear();
+            processDataList.Clear();
+            PopulateList();
+        }
+
+        private void processList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (e == null) throw new ArgumentNullException("e");
+            selectedProcessName = processList.GetItemText(processList.SelectedItem);
+            selectedProcess.Text = selectedProcessName + " is selected!";
+        }
+
+        private void MakeBorderless(object sender, EventArgs e)
+        {
+            RemoveBorder(selectedProcessName);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GotoSite("http://andrew.codeusa.net/");
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GotoSite("https://github.com/Codeusa/Borderless-Gaming");
+        }
+
+        public void GotoSite(string url) //open url
+        {
+            Process.Start(url);
+        }
+
+        private void UpdateList() // sets data sources
+        {
+            processList.DataSource = processDataList;
+        }
+    }
+}
