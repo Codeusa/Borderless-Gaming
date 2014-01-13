0b25e1adf2bdcc63305464c5e07885b5e4e982f4
Andrew Sampson <andrew@codeusa523.org>
2014-01-12 23:16:24 -0500

    Update Borderless.cs

--- Borderless.cs
+++ Borderless.cs
@@ -67,27 +67,6 @@ namespace BorderlessGaming
         private string _selectedProcessName;
         private string _selectedFavoriteProcess;
 
-        /// <summary>
-        ///     The MoveWindow function changes the position and dimensions of the specified window. For a top-level window, the
-        ///     position and dimensions are relative to the upper-left corner of the screen. For a child window, they are relative
-        ///     to the upper-left corner of the parent window's client area.
-        /// </summary>
-        /// <param name="hWnd">Handle to the window.</param>
-        /// <param name="X">Specifies the new position of the left side of the window.</param>
-        /// <param name="Y">Specifies the new position of the top of the window.</param>
-        /// <param name="nWidth">Specifies the new width of the window.</param>
-        /// <param name="nHeight">Specifies the new height of the window.</param>
-        /// <param name="bRepaint">
-        ///     Specifies whether the window is to be repainted. If this parameter is TRUE, the window receives
-        ///     a message. If the parameter is FALSE, no repainting of any kind occurs. This applies to the client area, the
-        ///     nonclient area (including the title bar and scroll bars), and any part of the parent window uncovered as a result
-        ///     of moving a child window.
-        /// </param>
-        /// <returns>
-        ///     If the function succeeds, the return value is nonzero.
-        ///     <para>If the function fails, the return value is zero. To get extended error information, call GetLastError.</para>
-        /// </returns>
-
         #endregion
         public Borderless()
         {
