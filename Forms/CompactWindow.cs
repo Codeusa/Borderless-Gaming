87c2f529c55f6fe3615eaeaaf2bb47146fe650ea
dmxt <dmxt@server.fake>
2014-01-14 23:33:05 +0400

    Modified main UI structure.
    Added Navbar with buttons and context menus.
    Added About Form.
    Moved GoToSite into Tools.cs

--- Forms/CompactWindow.cs
+++ Forms/CompactWindow.cs
@@ -13,7 +13,6 @@ namespace BorderlessGaming.Forms
     {
         private readonly List<string> _borderlessWindows = new List<string>();
         private readonly List<string> _processDataList = new List<string>();
-        private readonly List<string> _tempList = new List<string>();
         private IntPtr _formHandle;
         private bool _gameFound;
         private string _selectedFavoriteProcess;
@@ -49,7 +48,13 @@ namespace BorderlessGaming.Forms
 
         public static IntPtr FindWindowHandle(string processName, IntPtr ignoreHandle)
         {
-            var process = Process.GetProcesses().FirstOrDefault(p => p != null && p.ProcessName.Equals(processName, StringComparison.InvariantCultureIgnoreCase) && p.MainWindowHandle != IntPtr.Zero && p.MainWindowHandle != ignoreHandle && !string.IsNullOrEmpty(p.MainWindowTitle));
+            var process =
+                Process.GetProcesses()
+                    .FirstOrDefault(
+                        p =>
+                            p != null && p.ProcessName.Equals(processName, StringComparison.InvariantCultureIgnoreCase) &&
+                            p.MainWindowHandle != IntPtr.Zero && p.MainWindowHandle != ignoreHandle &&
+                            !string.IsNullOrEmpty(p.MainWindowTitle));
 
             return process != null ? process.MainWindowHandle : IntPtr.Zero;
         }
@@ -94,13 +99,13 @@ namespace BorderlessGaming.Forms
 
         private void _BackgroundWorkCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            if (!IsDisposed) { }
+            if (!IsDisposed)
+            {
+            }
         }
 
         private void PopulateList() //Adds active windows to the processDataList
         {
-            _tempList.Add("Refreshing...");
-            processList.DataSource = _tempList;
             var processlist = Process.GetProcesses();
 
             foreach (var process in
@@ -135,8 +140,8 @@ namespace BorderlessGaming.Forms
                 {
                     continue;
                 }
-                var pFoundWindow = proc.MainWindowHandle;
-                var style = Native.GetWindowLong(pFoundWindow, WindowLongIndex.Style);
+                var _processWindow = proc.MainWindowHandle;
+                var style = Native.GetWindowLong(_processWindow, WindowLongIndex.Style);
                 var HMENU = Native.GetMenu(proc.MainWindowHandle);
                 var count = Native.GetMenuItemCount(HMENU);
                 for (var i = 0; i < count; i++)
@@ -146,13 +151,20 @@ namespace BorderlessGaming.Forms
 
                 //force a redraw
                 Native.DrawMenuBar(proc.MainWindowHandle);
-                
-                Native.SetWindowLong(pFoundWindow, WindowLongIndex.Style, (style & ~(WindowStyleFlags.ExtendedDlgmodalframe | WindowStyleFlags.Caption | WindowStyleFlags.ThickFrame | WindowStyleFlags.Minimize | WindowStyleFlags.Maximize | WindowStyleFlags.SystemMenu | WindowStyleFlags.MaximizeBox | WindowStyleFlags.MinimizeBox | WindowStyleFlags.Border | WindowStyleFlags.ExtendedComposited)));
-                if (!_borderlessWindows.Contains(pFoundWindow.ToInt32().ToString()))
+
+                Native.SetWindowLong(_processWindow, WindowLongIndex.Style,
+                    (style &
+                     ~(WindowStyleFlags.ExtendedDlgmodalframe | WindowStyleFlags.Caption | WindowStyleFlags.ThickFrame |
+                       WindowStyleFlags.Minimize | WindowStyleFlags.Maximize | WindowStyleFlags.SystemMenu |
+                       WindowStyleFlags.MaximizeBox | WindowStyleFlags.MinimizeBox | WindowStyleFlags.Border |
+                       WindowStyleFlags.ExtendedComposited)));
+                if (!_borderlessWindows.Contains(_processWindow.ToInt32().ToString()))
                 {
-                    var bounds = Screen.FromHandle(pFoundWindow).Bounds;
-                    Native.SetWindowPos(pFoundWindow, 0, bounds.X, bounds.Y, bounds.Width, bounds.Height, SetWindowPosFlags.NoZOrder | SetWindowPosFlags.ShowWindow);
-                   _borderlessWindows.Add(pFoundWindow.ToInt32().ToString());
+                    var bounds = Screen.FromHandle(_processWindow).Bounds;
+                    Native.SetWindowPos(_processWindow, 0, bounds.X, bounds.Y, bounds.Width, bounds.Height,
+                        SetWindowPosFlags.NoZOrder | SetWindowPosFlags.ShowWindow);
+
+                    _borderlessWindows.Add(_processWindow.ToInt32().ToString());
                 }
                 _gameFound = true;
             }
@@ -175,15 +187,6 @@ namespace BorderlessGaming.Forms
             RemoveBorder(_selectedProcessName);
         }
 
-        private void BlogButtonClick(object sender, EventArgs e)
-        {
-            GotoSite("http://andrew.codeusa.net/");
-        }
-
-        private void GitHubButtonClick(object sender, EventArgs e)
-        {
-            GotoSite("https://github.com/Codeusa/Borderless-Gaming");
-        }
 
         public void GotoSite(string url) //open url
         {
@@ -192,10 +195,7 @@ namespace BorderlessGaming.Forms
 
         private void UpdateList() // sets data sources
         {
-
             processList.DataSource = _processDataList.OrderBy(x => x).ToList();
-          
-
         }
 
         private void workerTimer_Tick(object sender, EventArgs e)
@@ -211,7 +211,8 @@ namespace BorderlessGaming.Forms
         {
             if (_selectedProcessName == null || !Favorites.CanAdd(_selectedProcessName))
             {
-                MessageBox.Show("Unable to add " + _selectedProcessName + " already added!", "Uh oh!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Unable to add " + _selectedProcessName + " already added!", "Uh oh!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
@@ -219,20 +220,18 @@ namespace BorderlessGaming.Forms
                 Favorites.Save("./Favorites.json");
                 favoritesList.DataSource = null;
                 favoritesList.DataSource = Favorites.List;
-                MessageBox.Show(_selectedProcessName + " added to favorites", "Victory!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBox.Show(_selectedProcessName + " added to favorites", "Victory!", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
             }
         }
 
-        private void BugReportClick(object sender, EventArgs e)
-        {
-            GotoSite("https://github.com/Codeusa/Borderless-Gaming/issues");
-        }
 
         private void RemoveFavoriteButton(object sender, EventArgs e)
         {
             if (_selectedFavoriteProcess == null || !Favorites.CanRemove(_selectedFavoriteProcess))
             {
-                MessageBox.Show("Unable to remove " + _selectedFavoriteProcess + " does not exist!", "Uh oh!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Unable to remove " + _selectedFavoriteProcess + " does not exist!", "Uh oh!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
@@ -251,10 +250,6 @@ namespace BorderlessGaming.Forms
             _selectedFavoriteProcess = favoritesList.GetItemText(favoritesList.SelectedItem);
         }
 
-        private void SupportButtonClick(object sender, EventArgs e)
-        {
-            GotoSite("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TWHNPSC7HRNR2");
-        }
 
         protected override void OnResize(EventArgs e)
         {
@@ -283,6 +278,41 @@ namespace BorderlessGaming.Forms
         private void TrayIconExit(object sender, EventArgs e)
         {
             Environment.Exit(0);
+        }
+
+
+        private void ReportBug(object sender, EventArgs e)
+        {
+            GotoSite("https://github.com/Codeusa/Borderless-Gaming/issues");
+        }
+
+
+        private void OpenAboutForm(object sender, EventArgs e)
+        {
+            using (var aboutForm = new AboutForm())
+            {
+                aboutForm.ShowDialog();
+            }
+        }
+
+        private void ExitApplication(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void OpenPaypal(object sender, EventArgs e)
+        {
+            GotoSite("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TWHNPSC7HRNR2");
+        }
+
+        private void OpenContextMenuBlog(object sender, EventArgs e)
+        {
+            GotoSite("http://andrew.codeusa.net/");
+        }
+
+        private void OpenContextMenuSourceCode(object sender, EventArgs e)
+        {
+            GotoSite("https://github.com/Codeusa/Borderless-Gaming");
         }
     }
 }
--- Forms/MainWindow.Designer.cs
+++ Forms/MainWindow.Designer.cs
@@ -254,7 +254,7 @@
             this.tabFave.Location = new System.Drawing.Point(4, 22);
             this.tabFave.Name = "tabFave";
             this.tabFave.Padding = new System.Windows.Forms.Padding(3);
-            this.tabFave.Size = new System.Drawing.Size(576, 412);
+            this.tabFave.Size = new System.Drawing.Size(506, 412);
             this.tabFave.TabIndex = 1;
             this.tabFave.Text = "Favorites";
             this.tabFave.UseVisualStyleBackColor = true;
@@ -291,7 +291,6 @@
             this.MinimumSize = new System.Drawing.Size(530, 530);
             this.Name = "MainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Borderless Gaming";
             this.trayIconContextMenu.ResumeLayout(false);
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
