eb3eef6d542bdcc9bc1a867c20a3c23e055b607f
Codeusa <andrew@codeusa523.org>
2014-02-23 19:20:49 -0500

    Updated version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.6.0.0")]
-[assembly: AssemblyFileVersion("5.6.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
